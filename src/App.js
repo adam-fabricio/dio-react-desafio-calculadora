7f7586990d968e3bd221bb8086b223841e00a7e6
adam <adam@adam>
2024-10-07 19:17:59 -0300

    feat: desafio completo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from './components/Button';
+import Input from './components/Input'
 
-function App() {
+import { Container, Content, Row } from './styles';
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState("0");
+  const [operacao, setOperacao] = useState("0");
+  const [memoria, setMemoria] = useState("0");
+  const [flag, setFlag] = useState(true)
+
+  function clickButton(tecla)
+  {
+    if (!isNaN(parseFloat(tecla)) && isFinite(tecla))
+    {
+      digitaNumero(tecla);
+    } else {
+      teclaOperacaos(tecla)
+    }
+  }
+
+  function digitaNumero(tecla)
+  {
+    if (currentNumber === "0" || flag)
+    {
+      setCurrentNumber(tecla)
+      setFlag(false)
+    } else {
+      setCurrentNumber(currentNumber + tecla)
+    }
+  }
+
+  function teclaOperacaos(tecla)
+  {
+    switch (tecla){
+      case "C":
+        limpar();
+        break;
+      case "=":
+        calculaResultado();
+        break;
+      default:
+        setOperacao(tecla)
+        if (operacao !== "0"){
+          calculaResultado();
+        } else {
+          setMemoria(currentNumber);          
+        }
+        setFlag(1);
+    }
+
+  }
+
+  function limpar()
+  {
+    setCurrentNumber('0');
+    setMemoria("0");
+    setOperacao("0");
+    setFlag("0");
+  }
+
+  function calculaResultado()
+  {
+    const val1 = Number(memoria);
+    const val2 = Number(currentNumber);
+    let resultado;
+    switch (operacao) {
+      case "+":
+        resultado = val1 + val2;
+        break;
+      case "-":
+        resultado = val1 - val2;
+        break
+      case "*":
+        resultado = val1 * val2;
+        break;
+      default:
+        resultado = val1 / val2;
+    }
+    setCurrentNumber(resultado);
+    setMemoria(resultado)
+    setFlag(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="/" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="*" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="C" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="=" onClick={(e) => clickButton(e.target.innerText)}/>
+        </Row>
+        <Row>
+          <Button label="7" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="8" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="9" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="-" onClick={(e) => clickButton(e.target.innerText)}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="5" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="6" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="+" onClick={(e) => clickButton(e.target.innerText)}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="2" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="3" onClick={(e) => clickButton(e.target.innerText)}/>
+          <Button label="0" onClick={(e) => clickButton(e.target.innerText)}/>
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    flex:1;
+    padding: 20px;
+    border: 1px solid #000000;
+    background-color: #202020;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+
+    &:hover {
+        opacity: 0.9;
+    }
+
+`
